e6e0ffaa4afc84a0aa72e3d4a795215bdeae1cde
KirthiKumarHM <Kirthihm@users.noreply.github.com>
2019-07-13 19:31:49 +0530

    Added FullstachWithAngular project to the git repository

---
+++ FullstackWithAngular/FullstackWithAngular/src/main/java/com/kk/fullstack/FullstackWithAngular/FullstackWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.kk.fullstack.FullstackWithAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackWithAngularApplication.class, args);
+	}
+
+}
